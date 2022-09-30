3ddd181c38e1cd2c690cc20ebaaaa980576f828f
SpeedCrash100 <SpeedCrash100@yandex.ru>
2022-09-30 11:47:15 +0000

    Use blocking API for sending commands

--- src/dma_display_connector.rs
+++ src/dma_display_connector.rs
@@ -9,14 +9,24 @@ const DISPLAY_BUFFER_SIZE: usize = 128 * 64 / 8 + 1; // Display 128x64 + 1 byte 
 const COMMAND_BUFFER_SIZE: usize = 8;
 const I2C_ADDRESS: u8 = 0x3C;
 
-pub struct DMAI2cInterface<I2C: I2CMasterWriteDMA + 'static> {
+pub trait I2CWrite {
+    fn write(&mut self, addr: u8, buf: &[u8]) -> nb::Result<(), hal::i2c::Error>;
+}
+
+impl I2CWrite for crate::app::I2c1Handle {
+    fn write(&mut self, addr: u8, buf: &[u8]) -> nb::Result<(), hal::i2c::Error> {
+        crate::app::I2c1Handle::write(self, addr, buf)
+    }
+}
+
+pub struct DMAI2cInterface<I2C: 'static> {
     display_buffer: [u8; DISPLAY_BUFFER_SIZE], // Display 128x64 + 1 byte for DataByte
     command_buffer: [u8; COMMAND_BUFFER_SIZE],
 
     i2c: &'static Mutex<RefCell<I2C>>,
 }
 
-impl<I2C: I2CMasterWriteDMA + 'static> DMAI2cInterface<I2C> {
+impl<I2C: I2CMasterWriteDMA + I2CWrite + 'static> DMAI2cInterface<I2C> {
     pub fn new(bus: &'static Mutex<RefCell<I2C>>) -> Self {
         Self {
             display_buffer: [0x40; DISPLAY_BUFFER_SIZE],
@@ -26,7 +36,7 @@ impl<I2C: I2CMasterWriteDMA + 'static> DMAI2cInterface<I2C> {
     }
 }
 
-impl<I2C: I2CMasterWriteDMA + 'static> WriteOnlyDataCommand for DMAI2cInterface<I2C> {
+impl<I2C: I2CMasterWriteDMA + I2CWrite + 'static> WriteOnlyDataCommand for DMAI2cInterface<I2C> {
     fn send_commands(&mut self, cmd: DataFormat<'_>) -> Result<(), DisplayError> {
         match cmd {
             DataFormat::U8(slice) => {
@@ -34,9 +44,8 @@ impl<I2C: I2CMasterWriteDMA + 'static> WriteOnlyDataCommand for DMAI2cInterface<
 
                 let res = nb::block!(critical_section::with(|cs| {
                     let mut i2c = self.i2c.borrow(cs).borrow_mut();
-                    unsafe {
-                        i2c.write_dma(I2C_ADDRESS, &self.command_buffer[..=slice.len()], None)
-                    }
+
+                    i2c.write(I2C_ADDRESS, &self.command_buffer[..=slice.len()])
                 }));
 
                 if let Err(_) = res {
@@ -54,12 +63,16 @@ impl<I2C: I2CMasterWriteDMA + 'static> WriteOnlyDataCommand for DMAI2cInterface<
             DataFormat::U8(slice) => {
                 self.display_buffer[1..=slice.len()].copy_from_slice(&slice[0..slice.len()]);
 
-                let _res = nb::block!(critical_section::with(|cs| {
+                let res = nb::block!(critical_section::with(|cs| {
                     let mut i2c = self.i2c.borrow(cs).borrow_mut();
                     unsafe {
                         i2c.write_dma(I2C_ADDRESS, &self.display_buffer[..=slice.len()], None)
                     }
                 }));
+
+                if let Err(_) = res {
+                    return Err(DisplayError::BusWriteError);
+                }
 
                 Ok(())
             }
